7f330bda6982c291a47e645ea9b34fd91df17a43
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2021-11-06 19:02:34 -0500

    Updated index.js and documentation.html
    Added more in depth explanation to documentation.html
    
    Added genres to movies
    
    Updated and tested endpoints

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+//imported modules
 const express = require('express'),
-  morgan = require('morgan');
+morgan = require('morgan');
+
 
 const app = express();
 
 app.use(morgan('common'));
-
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix! This is a database dediacated to sharing info on my favorite films ranging from action to comedy to documentaries!');
@@ -14,51 +16,61 @@ app.get('/', (req, res) => {
 let topMovies = [
   {
     title: 'Spider-Man:Far From Home',
-    released: '2019'
+    genre:'Action/Adventure' ,
+    released: { year: '2019' } ,
     
   },
   {
     title: 'Beautiful Noise',
-    released: '2014'
+    genre:'Documentary' ,
+    released: { year:'2014' } ,
   },
  
   {
     title: 'The Decline of Western Civilization',
-    released: '1981'
+    genre:'Documentary' ,
+    released: { year: '1981' } ,
   },
 
   {
     title: 'Clerks',
-    released: '1994'
+    genre:'Comedy' ,
+    released: { year: '1994' } ,
   },
 
   {
     title: 'Private Parts',
-    released: '1997'
+    genre:'Comedy' ,
+    released: { year:'1997' } ,
   },
 
   {
     title: 'Zootopia',
-    released: '2016'
+    genre:'Animated' ,
+    released: { year:'2016' } ,
   },
   {
     title: ' The Wrestler',
-    released: '2008'
+    genre:'Drama' ,
+    released: { year: '2008'} ,
   },
  
   {
     title: 'Dogtown and Z-Boys',
-    released: '2001'
+    genre:'Drama' ,
+    released: { year:'2001' } ,
   },
 
   {
     title: 'Deadpool',
-    released: '2016'
+    genre:'Action/Adventure' ,
+    released: { year: '2016' } ,
   },
 
   {
     title: 'Beyond The Mat',
-    released: '1999'
+    genre:'Documentary' ,
+    released: { year:'1999' } ,
   }
 ];
 
@@ -69,6 +81,35 @@ app.get('/movies', (req, res) => {
 app.get('/documentation.html', (req, res) => {
 });
 
+app.get('/movies/:title/genre', (req, res) => {
+  res.send('Successful GET request returning data about a genre.');
+});
+
+app.get('/movies/released/:year', (req, res) => {
+  res.send('Successful GET request returning data about a release date.');
+});
+
+app.post('/newUser', (req, res) => {
+  res.send('Successful POST request - new user is registered');
+});
+
+app.put('/newUser/:id/info', (req, res) => {
+  res.send('Successful PUT request - user info is updated');
+});
+
+app.post('/newUser/:id/favorites', (req, res) => {
+  res.send('Successful POST request - user added a movie to their favorites');
+});
+
+app.delete('/newUser', (req, res) => {
+  res.send('Successful DELETE request - user has deregistered');
+});
+
+app.delete('/newUser/:id/favorites', (req, res) => {
+  res.send(
+    'Successful DELETE request movie has been deleted from users list of favorites.'
+  );
+});
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
