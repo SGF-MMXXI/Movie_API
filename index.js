95386296cb30483e05bfeea6c981c93edb914caa
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2021-12-01 22:43:29 -0600

    converted mongo data to mongoose models
    updated models.js
    updated index.js
    updated documentation.html

--- index.js
+++ index.js
@@ -1,115 +1,210 @@
 //imported modules
+const mongoose = require('mongoose');
+
+const models = require('./models.js');
+
 const express = require('express'),
 morgan = require('morgan');
 
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const movies = models.movies;
+const users = models.users;
+const Genre = models.Genre;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(express.json());
 
+
+//MyFlixHomepage
 app.get('/', (req, res) => {
   res.send('Welcome to myFlix! This is a database dediacated to sharing info on my favorite films ranging from action to comedy to documentaries!');
 });
 
-let topMovies = [
-  {
-    title: 'Spider-Man:Far From Home',
-    genre:'Action/Adventure' ,
-    released: { year: '2019' } ,
-    
-  },
-  {
-    title: 'Beautiful Noise',
-    genre:'Documentary' ,
-    released: { year:'2014' } ,
-  },
- 
-  {
-    title: 'The Decline of Western Civilization',
-    genre:'Documentary' ,
-    released: { year: '1981' } ,
-  },
-
-  {
-    title: 'Clerks',
-    genre:'Comedy' ,
-    released: { year: '1994' } ,
-  },
-
-  {
-    title: 'Private Parts',
-    genre:'Comedy' ,
-    released: { year:'1997' } ,
-  },
-
-  {
-    title: 'Zootopia',
-    genre:'Animated' ,
-    released: { year:'2016' } ,
-  },
-  {
-    title: ' The Wrestler',
-    genre:'Drama' ,
-    released: { year: '2008'} ,
-  },
- 
-  {
-    title: 'Dogtown and Z-Boys',
-    genre:'Drama' ,
-    released: { year:'2001' } ,
-  },
-
-  {
-    title: 'Deadpool',
-    genre:'Action/Adventure' ,
-    released: { year: '2016' } ,
-  },
-
-  {
-    title: 'Beyond The Mat',
-    genre:'Documentary' ,
-    released: { year:'1999' } ,
-  }
-];
-
+//Return All movies
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
+    movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
+//Return All Users
+  app.get("/users",function (req, res){
+    users.find()
+    .then(function (users) {
+      res.status(201).json(users);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+  });
+
+//Return Single User by Username
+  app.get('/users/:username', (req, res) => {
+    users.findOne({username: req.params.username})
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
 
 app.get('/documentation.html', (req, res) => {
 });
 
-app.get('/movies/:title/genre', (req, res) => {
-  res.send('Successful GET request returning data about a genre.');
+//Get single movie by title
+app.get('/movies/:Title', (req, res) => {
+    movies.findOne({Title: req.params.Title})
+        .then((movie) => {
+            res.json(movie);
+        })
+        .catch((err) => {//error callback
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/movies/released/:year', (req, res) => {
-  res.send('Successful GET request returning data about a release date.');
+//Return movies by Genre
+app.get('/movies/genres/:name', (req, res) => {
+    movies.find({ "Genre.Name" : req.params.name })
+        .then((genre) => {
+            res.json(genre);
+        })
+        .catch ((err) => {
+            console.error(err);
+            res.status(500).send("Error: " + err);
+        });
 });
 
-app.post('/newUser', (req, res) => {
-  res.send('Successful POST request - new user is registered');
+//Return Director Data
+app.get('/movies/director/:Name', (req, res) => {
+  movies.findOne({ "Director.Name" : req.params.Name })
+    .then((movies) => {
+      res.json(movies.Director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.put('/newUser/:id/info', (req, res) => {
-  res.send('Successful PUT request - user info is updated');
+//Add User
+app.post('/users', (req, res) => {
+  users.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.username + 'already exists');
+      } else {
+        users.create({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) 
+          })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.post('/newUser/:id/favorites', (req, res) => {
-  res.send('Successful POST request - user added a movie to their favorites');
+//Update User Data
+app.put('/users/:username', (req, res) => {
+  users.findOneAndUpdate(
+    {username: req.params.username},
+    {$set: { 
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email,
+    Birthday: req.body.Birthday,
+    }
+},
+{new: true },
+(err, updatedUser) => {
+  if (err) {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  } else {
+    res.json(updatedUser);
+  }
+  });
+});
+  
+  //Add Favorite Movie To User Profile
+app.post('/users/:username/movies/:MovieID', (req, res) => {
+    users.findOneAndUpdate({ username: req.params.username},
+        {$addToSet: { FavoriteMovies: req.params.MovieID}
+    },
+    { new: true},
+    (err, updatedUser) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
-app.delete('/newUser', (req, res) => {
-  res.send('Successful DELETE request - user has deregistered');
+//Delete User
+app.delete('/users/:username', (req, res) => {
+  users.findOneAndRemove({ 
+    username:req.params.username 
+  })
+  .then((user) => {
+    if (!user) {
+      res.status(400).send(req.params.username + " was not found");
+
+    } else {
+      res.status(200).send(req.params.username + " was deleted.");
+    
+    }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error" + err);
+    });
 });
 
-app.delete('/newUser/:id/favorites', (req, res) => {
-  res.send(
-    'Successful DELETE request movie has been deleted from users list of favorites.'
-  );
+//Delete User Favorite Movie
+app.delete('/users/:username/movies/:MovieID', (req, res) => {
+    users.findOneAndUpdate({ username: req.params.username},
+        {$pull: { FavoriteMovies: req.params.MovieID}
+    },
+    { new: true},
+    (err, updatedUser) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
---
+++ models.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+      Name: String,
+      Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  username: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true},
+  Birthday: Date,
+FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'movies' }]
+});
+
+
+
+let movies = mongoose.model('movies', movieSchema);
+let users = mongoose.model('users', userSchema);
+
+
+
+module.exports.movies = movies;
+module.exports.users = users;
+
