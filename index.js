6d612ee8dd8d59e86f8d47899f6d73b76909d90c
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2021-12-12 22:52:55 -0600

    updated mongoose connect code

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const movies = models.movies;
 const users = models.users;
 const Genre = models.Genre;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(morgan('common'));
 app.use(express.static('public'));
@@ -234,3 +236,6 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
  console.log('Listening on Port ' + port);
 });
+
+
+
