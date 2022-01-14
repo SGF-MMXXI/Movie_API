46d31751bc6fc3bde1503a9148340cb5ed3d6b43
SGF-MMXXI <88549986+SGF-MMXXI@users.noreply.github.com>
2022-01-14 09:49:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/', (req, res) => {
 });
 
 //Return All movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate("jwt", { session: false }), (req, res) => {
     movies.find()
       .then((movies) => {
         res.status(201).json(movies);
